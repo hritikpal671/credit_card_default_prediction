3cd67697baa01d0b85985556956ca8d9400924f7
Hritik <hritikpal671@gmail.com>
2023-02-10 01:53:54 +0530

    project completed

---
+++ main.py
@@ -0,0 +1,13 @@
+
+from project_files.pipeline.training_pipeline import start_training_pipeline
+from project_files.pipeline.batch_prediction import start_batch_prediction
+
+file_path="/config/workspace/UCI_Credit_Card.csv"
+print(__name__)
+if __name__=="__main__":
+     try:
+          start_training_pipeline()
+          output_file = start_batch_prediction(input_file_path=file_path)
+          print(output_file)
+     except Exception as e:
+          print(e)
